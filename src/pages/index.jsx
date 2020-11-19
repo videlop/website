30a0acb33959ecdcec9b0065df7c053bdd876b06
Wello6143 <wello6143@outlook.com>
2020-11-19 18:11:30 +0700

    Pushing some small works

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 
-const index = ({ data }) => {
+const index = () => {
 	return ( 
 	<Layout>
-		<div className="container text-center">
+		<div className="container text-center mb-3">
 			<p className="mb-0 text-5xl">Cộng đồng Videlop</p>
 			<p className="text-lg">một cộng đồng nghiên cứu và lập trình các công nghệ mở</p>
 		</div>
-		<div className="">
-			
+		<div className="container mx-auto">
+
 		</div>
 	</Layout>
 	)
