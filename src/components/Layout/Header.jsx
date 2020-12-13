04511f7c21f6b5d9f5206ed65bdf7a5826ed1af0
Trung Nhan Dang <wello6143@outlook.com>
2020-12-13 22:23:02 +0700

    Update some dependency packages

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
 		<header className="text-white md:px-16 px-6 flex flex-wrap items-center py-3">
 			<div className="text-2xl mr-10">Logo Videlop</div>
 			<nav className="site-nav md:flex md:pt-0 hidden md:w-auto w-full">
-			<label htmlFor="menu-toggle" className="pointer-cursor md:hidden block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
+			<label htmlFor="menu-toggle" className="pointer-cursor md:hidden block"><svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
 			<input className="hidden" type="checkbox" id="menu-toggle" />
 				<Link className="md:p-4 border-transparent" href="/"><a>Trang chủ</a></Link>
-				<Link className="md:p-4 border-transparent" href="/blog"><a>Blog</a></Link>
+				<Link className="md:p-4 border-transparent" href="https://blog.videlop.com"><a>Blog</a></Link>
 				<Link className="md:p-4 border-transparent" href="/kb"><a>Kho kiến thức</a></Link>
-				<Link className="md:p-4 border-transparent" href="/kb"><a>Kho kiến thức</a></Link>
+				<Link className="md:p-4 border-transparent" href="/contact"><a>Liên hệ</a></Link>
 
 			</nav>
 		</header>
---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const index = () => {
+	return ( 
+	<Layout>
+        
+	</Layout>
+	)
+}
+
+export default index
